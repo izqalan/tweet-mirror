0c7f3e097edb8667eb843b491a4e8720ed261af2
izqalan <izqalan@gmail.com>
2020-03-08 11:36:20 +0800

    fix #1

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 require('dotenv').config()
 import Twit from 'twit';
 import config from './config';
-import translate from '@vitalets/google-translate-api';
+import translate, { languages } from '@vitalets/google-translate-api';
 
 var T = new Twit(config);
 
-var stream = T.stream('statuses/filter', { follow: [process.env.TARGET_USER_ID], tweet_mode: 'extended' })
+var stream = T.stream('statuses/filter', { language: ['en'], follow: [process.env.TARGET_USER_ID], tweet_mode: ['extended'] })
 
 stream.on('tweet', async function (data) {
   var tweet = data.text;
+  var rt = data.retweeted_status ? true : false;
+  var mention = data.in_reply_to_screen_name ? true : false;
+  console.group(['Incoming tweet'])
   console.log(`[${process.env.TARGET_USER_ID}] tweeted: ${tweet}`)
+  console.log(`[${process.env.TARGET_USER_ID}] is a RT? ${rt}`)
+  console.log(`[${process.env.TARGET_USER_ID}] is a mention? ${mention}`)
+  console.groupEnd()
+  if (rt == false && mention == false) {
+    translate(tweet, { to: 'ja' }).then(res => {
+      tweetThis(res.text)
+    }).catch(err => {
+      console.error(err);
+    });
+  } else {
+    console.warn(`[${process.env.TARGET_USER_ID}] cannot translate; condition not met!`)
+  }
 
-  translate(tweet, { to: 'ja' }).then(res => {
-
-    tweetThis(res.text)
-
-  }).catch(err => {
-    console.error(err);
-  });
 })
 
 function tweetThis(tweet) {
@@ -30,7 +38,7 @@ function tweetThis(tweet) {
     if (err) {
       console.log(err)
     } else {
-      console.log('tweeted!')
+      console.log(`[${process.env.TARGET_USER_ID}] tweeted!`)
     }
   }
 }
