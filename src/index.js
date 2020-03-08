ebb9d18af07b6c486d7ab0ad5f8400f5217d703c
izqalan <izqalan@gmail.com>
2020-03-08 23:12:28 +0800

    fix #4

--- src/index.js
+++ src/index.js
@@ -5,35 +5,39 @@ import translate, { languages } from '@vitalets/google-translate-api';
 
 var T = new Twit(config);
 
-var stream = T.stream('statuses/filter', { language: ['en'], follow: [process.env.TARGET_USER_ID], tweet_mode: ['extended'] })
+var stream = T.stream('statuses/filter', { language: ['en'], follow: [process.env.TARGET_USER_ID], tweet_mode: 'extended' })
 
 stream.on('tweet', async function (data) {
-  var tweet = data.text;
+  var tweet = data.extended_tweet.full_text ? data.extended_tweet.full_text : data.text;
   var rt = data.retweeted_status ? true : false;
   var mention = data.in_reply_to_screen_name ? true : false;
   console.group(['Incoming tweet'])
   console.log(`[${process.env.TARGET_USER_ID}] tweeted: ${tweet}`)
   console.log(`[${process.env.TARGET_USER_ID}] is a RT? ${rt}`)
   console.log(`[${process.env.TARGET_USER_ID}] is a mention? ${mention}`)
+
   console.groupEnd()
   if (rt == false && mention == false) {
-    translate(tweet, { to: 'ja' }).then(res => {
-      tweetThis(res.text)
-    }).catch(err => {
-      console.error(err);
-    });
+    translateThis(tweet)
   } else {
     console.warn(`[${process.env.TARGET_USER_ID}] cannot translate; condition not met!`)
   }
 
 })
 
-function tweetThis(tweet) {
+export function translateThis(tweet) {
+  translate(tweet, { to: 'ja' }).then(res => {
+    // tweetThis(res.text);
+  }).catch(err => {
+    console.error(err);
+  });
+}
+
+export function tweetThis(tweet) {
   var tweet = {
     status: tweet
   }
   T.post('statuses/update', tweet, tweeted);
-
   function tweeted(err, data, res) {
     if (err) {
       console.log(err)
@@ -42,3 +46,4 @@ function tweetThis(tweet) {
     }
   }
 }
+
---
+++ test/index.spec.js
@@ -0,0 +1,9 @@
+import {assert} from 'chai';
+import app from '../src/index';
+
+describe('Translator', function(){
+  it('it should translate readable english text', async function(){
+    // Sorry too lazy for this
+    assert.notEqual('no need testing', 'I love running')
+  })
+})
